12a2fb732717a3f0798fa66dcd1f7336e51e9b13
KotsuCoder <whoami@kotsu.red>
2026-01-07 00:04:56 -0700

    feat: print error for invalid commands

--- shell.py
+++ shell.py
@@ -5,6 +5,8 @@ def main():
     Main function to run the Vibe Coded Shell.
     """
     print("$ ", end="")
+    command = input()
+    print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
